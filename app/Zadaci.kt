e42ea7634a77775b2305aac82f3dd7b7dea0705b
jana2407 <jana.cvrtila@gmail.com>
2023-02-05 15:03:00 +0100

    Initial commit

---
+++ app/Zadaci.kt
@@ -0,0 +1,9 @@
+package com.example.petcare
+
+data class Zadaci (
+        val lijekovi: String "",
+        val kupanje: String "",
+        val sisanje: String "",
+        val cesljanje: String ""
+
+        )
